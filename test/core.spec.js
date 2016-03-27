6b3c7142445554e96b9ab9d3c20d1e9913ee579a
Jamón Serrano <honti.viktor@gmail.com>
2016-03-27 18:16:06 +0200

    Core tests: $$

--- test/core.spec.js
+++ test/core.spec.js
@@ -1,4 +1,16 @@
 describe("Core", function () {
+    before(function () {
+        fixture.setBase("test/fixtures");
+    });
+
+    function addFixture () {
+        fixture.load('basic_elements.html');
+    }
+
+    function removeFixture () {
+        fixture.cleanup();
+    }
+
     describe("globals", function () {
         it("should expose Jamon as a global variable", function () {
             expect(Jamon).to.exist;
@@ -20,18 +32,6 @@ describe("Core", function () {
     });
 
     describe("$", function () {
-        before(function () {
-            fixture.setBase("test/fixtures");
-        });
-
-        function addFixture () {
-            fixture.load('basic_elements.html');
-        }
-
-        function removeFixture () {
-            fixture.cleanup();
-        }
-
         it("should work without arguments", function () {
             var result = $();
 
@@ -42,25 +42,13 @@ describe("Core", function () {
         it("should work with a selector string", function () {
             addFixture();
 
-            var selector = "#id1";
+            var selector = "div";
             var qSResult = document.querySelector(selector);
             var result = $(selector);
 
             expect(result).to.be.an.instanceof(Jamon);
             expect(result).to.have.lengthOf(1);
             expect(result[0]).to.equal(qSResult);
-
-            removeFixture();
-        });
-
-        it("should return only one element", function () {
-            addFixture();
-
-            var result = $("div");
-            var qSAResult = document.querySelectorAll("div")[0];
-
-            expect(result).to.have.lengthOf(1);
-            expect(result[0]).to.equal(qSAResult);
 
             removeFixture();
         });
@@ -105,5 +93,84 @@ describe("Core", function () {
     });
 
     describe("$$", function () {
+
+        it("should work without arguments", function () {
+            var results = $$();
+
+            expect(results).to.be.an.instanceof(Jamon);
+            expect(results).to.have.lengthOf(0);
+        });
+
+        it("should work with a selector string", function () {
+            addFixture();
+
+            var selector = "div";
+            var qSAResults = document.querySelectorAll(selector);
+            var results = $$(selector);
+
+            expect(results).to.be.an.instanceof(Jamon);
+
+            removeFixture();
+        });
+
+        it("should return multiple elements", function () {
+            addFixture();
+
+            var selector = "div";
+            var qSAResults = document.querySelectorAll(selector);
+            var results = $$(selector);
+            var length = results.length;
+            var i = 0;
+
+            expect(length).to.equal(qSAResults.length);
+            for (; i < length; i++) {
+                expect(results[i]).to.equal(qSAResults[i]);
+            }
+
+            removeFixture();
+        });
+
+        it("should work with a NodeList", function () {
+            addFixture();
+
+            var nodeList = document.querySelectorAll("div");
+            var results = $$(nodeList);
+            var length = results.length;
+            var i = 0;
+
+            expect(results).to.be.an.instanceof(Jamon);
+            expect(length).to.equal(nodeList.length);
+            for (; i < length; i++) {
+                expect(results[i]).to.equal(nodeList[i]);
+            }
+
+            removeFixture();
+        });
+
+        it("should work with a HTMLCollection", function () {
+            addFixture();
+
+            var htmlCollection = document.getElementsByTagName("div");
+            var results = $$(htmlCollection);
+            var length = results.length;
+            var i = 0;
+
+            expect(results).to.be.an.instanceof(Jamon);
+            expect(length).to.equal(htmlCollection.length);
+            for (; i < length; i++) {
+                expect(results[i]).to.equal(htmlCollection[i]);
+            }
+
+            removeFixture();
+        });
+
+        it("should work on a Jamón instance", function () {
+            var original = $$();
+            var result = $$(original);
+
+            expect(result).to.be.an.instanceof(Jamon);
+            expect(result).to.equal(original);
+        });
     });
+
 });
