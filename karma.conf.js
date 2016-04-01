3825b7e22e5dea6e0898adfb318102f0261a2bf5
Jamón Serrano <honti.viktor@gmail.com>
2016-04-01 14:30:59 +0200

    Removed unneeded dev dependencies. Lint before tests

--- karma.conf.js
+++ karma.conf.js
@@ -30,8 +30,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.html'   : ['html2js'],
-      '**/*.json'   : ['json_fixtures']
+      'jamon.js': ['eslint'],
+      '**/*.html': ['html2js']
     },
 
 
