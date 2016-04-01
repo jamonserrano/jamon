4f7bb5b3e4154f7f85dba2bdb60c34265bd0f1b5
Jamón Serrano <honti.viktor@gmail.com>
2016-04-01 18:40:18 +0200

    Remove mocha-debug from Karma configuration too

--- karma.conf-sauce.js
+++ karma.conf-sauce.js
@@ -36,7 +36,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha-debug', 'mocha', 'fixture', 'chai-dom', 'chai'],
+    frameworks: ['mocha', 'fixture', 'chai-dom', 'chai'],
 
 
     // list of files / patterns to load in the browser
@@ -50,6 +50,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'jamon.js': ['eslint'],
       '**/*.html'   : ['html2js']
     },
 
