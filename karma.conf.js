27c68466a4efe267c2f94da2f60a0c5707b4e930
Jamón Serrano <honti.viktor@gmail.com>
2016-05-07 10:33:05 +0200

    Local test coverage reporting

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'jamon.js': ['eslint'],
+      'jamon.js': ['eslint', 'coverage'],
       '**/*.html': ['html2js']
     },
 
@@ -37,9 +37,13 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
-
+    
+    coverageReporter: {
+      type : 'text-summary'
+    },
+    
     // web server port
     port: 9876,
 
