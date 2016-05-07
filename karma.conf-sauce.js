aaaff4a6f664c6a1a07184234360dbdc25954447
Jamón Serrano <honti.viktor@gmail.com>
2016-05-07 10:47:57 +0200

    Saucelabs should send coverage report to Coveralls

--- karma.conf-sauce.js
+++ karma.conf-sauce.js
@@ -49,7 +49,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'jamon.js': ['eslint'],
+      'jamon.js': ['eslint', 'coverage'],
       '**/*.html'   : ['html2js']
     },
 
@@ -57,7 +57,12 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots', 'saucelabs'],
+    reporters: ['dots', 'saucelabs', 'coverage', 'coveralls'],
+    
+    coverageReporter: {
+      type : 'lcov',
+      dir: 'coverage/'
+    },
 
     // web server port
     port: 9876,
